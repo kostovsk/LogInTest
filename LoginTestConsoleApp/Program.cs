5e05a9512a324a74637e266262ff4f0bafb43bd3
Michael Kostov <kostovmichael@yahoo.com>
2020-04-10 13:32:47 -0700

    adding console app and windows forms app

---
+++ LoginTestConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LoginTestConsoleApp
+{
+   class Program
+   {
+      static void Main(string[] args)
+      {
+         Console.WriteLine("Hello World!");
+      }
+   }
+}
