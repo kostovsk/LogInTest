2c2c184aaf70c8b8816720005ec2f3fdedf63287
Stoycho Kostov <stoichokostov@yahoo.com>
2020-04-16 12:13:10 -0500

    Update Program.cs

--- LoginTestConsoleApp/Program.cs
+++ LoginTestConsoleApp/Program.cs
@@ -3,42 +3,56 @@ using System.Collections.Generic;
 
 namespace LoginTestConsoleApp
 {
-   class Program
-   {
-      static void Main(string[] args)
-      {
+    class Program
+    {
+        static void Main(string[] args)
+        {
 
-         Console.WriteLine("Enter your username:");
-         string inputUsername = Console.ReadLine();
+            Console.WriteLine("Enter your username:");
+            string inputUsername = Console.ReadLine();
 
-         Console.WriteLine("Enter your password:");
-         string inputPassword = Console.ReadLine();
+            Console.WriteLine("Enter your password:");
+            string inputPassword = Console.ReadLine();
 
-         string[][] users = GetUserPasswordArray();
+            string[][] users = GetUserPasswordArray();
 
-         //TODO: write your code here to check inputUsername and inputPassword against the values in the users array
-         Console.ReadLine();
-      }
+            foreach (string[] array in users)
+            {
+                foreach (string item in array)
+                {
+                    if (item.Contains(inputUsername))
+                    {
+                        Console.WriteLine("Username is correct.");
+                    }
+                    if (item.Contains(inputPassword))
+                    {
+                        Console.WriteLine("Password is correct.");
+                    }
+                }
+            }
 
-      private static string[][] GetUserPasswordArray()
-      {
-         string[][] arr = new string[3][];
+            Console.ReadLine();
+        }
 
-         arr[0] = new string[2];
-         arr[0][0] = "kostovsk";
-         arr[0][1] = "123";
+        private static string[][] GetUserPasswordArray()
+        {
+            string[][] arr = new string[3][];
 
-         arr[1] = new string[2];
-         arr[1][0] = "skostov";
-         arr[1][1] = "456";
+            arr[0] = new string[2];
+            arr[0][0] = "kostovsk";
+            arr[0][1] = "123";
+
+            arr[1] = new string[2];
+            arr[1][0] = "skostov";
+            arr[1][1] = "456";
 
 
-         arr[2] = new string[2];
-         arr[2][0] = "stoichokostov";
-         arr[2][1] = "789";
+            arr[2] = new string[2];
+            arr[2][0] = "stoichokostov";
+            arr[2][1] = "789";
 
-         return arr;
-      }
+            return arr;
+        }
 
-   }
+    }
 }
