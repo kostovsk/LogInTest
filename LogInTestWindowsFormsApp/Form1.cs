681bf2bbf87be19b473ddfd0dd74291d6322b342
Stoycho Kostov <stoichokostov@yahoo.com>
2020-05-11 15:54:21 -0500

    Update Form1.cs

--- LogInTestWindowsFormsApp/Form1.cs
+++ LogInTestWindowsFormsApp/Form1.cs
@@ -10,168 +10,168 @@ using System.Windows.Forms;
 
 namespace LogInTestWindowsFormsApp
 {
-    public partial class Form1 : Form
-    {
-        public Form1()
-        {
-            InitializeComponent();
-            textBox1.KeyDown += textBox1_KeyDown;
-            textBox2.KeyDown += textBox2_KeyDown;
-            textBox2.PasswordChar = '●';
-        }
+   public partial class Form1 : Form
+   {
+      public Form1()
+      {
+         InitializeComponent();
+         textBox1.KeyDown += textBox1_KeyDown;
+         textBox2.KeyDown += textBox2_KeyDown;
+         textBox2.PasswordChar = '●';
+      }
 
-        private void Form1_Load(object sender, EventArgs e)
-        {
+      private void Form1_Load(object sender, EventArgs e)
+      {
 
-        }
+      }
 
-        private void textBox1_TextChanged(object sender, EventArgs e)
-        {
-            
-        }
+      private void textBox1_TextChanged(object sender, EventArgs e)
+      {
 
-        private void textBox2_TextChanged(object sender, EventArgs e)
-        {
+      }
 
-        }
+      private void textBox2_TextChanged(object sender, EventArgs e)
+      {
 
-        private void textBox1_KeyDown(object sender, KeyEventArgs e)
-        {
-            if (e.KeyCode == Keys.Enter)
+      }
+
+      private void textBox1_KeyDown(object sender, KeyEventArgs e)
+      {
+         if (e.KeyCode == Keys.Enter)
+         {
+            textBox2.Focus();
+         }
+      }
+
+      private void textBox2_KeyDown(object sender, KeyEventArgs e)
+      {
+         if (e.KeyCode == Keys.Enter)
+         {
+            button1.PerformClick();
+         }
+      }
+
+      public static readonly int maxFailedLoginAttempts = 3;
+
+      bool userNameIsValid = false;
+      bool passwordIsValid = false;
+      int failedLoginAttempts = 0;
+      int failedPassAttempts = 0;
+
+      private void button1_Click(object sender, EventArgs e)
+      {
+
+         string inputUsername = textBox1.Text;
+         string inputPassword = textBox2.Text;
+
+         if (failedLoginAttempts < maxFailedLoginAttempts)
+         {
+            int indexOfUserName = getUsernameIndex(inputUsername);
+            if (indexOfUserName > -1)
             {
-                textBox2.Focus();
+               userNameIsValid = true;
             }
-        }
 
-        private void textBox2_KeyDown(object sender, KeyEventArgs e)
-        {
-            if (e.KeyCode == Keys.Enter)
+
+            if (userNameIsValid == true)
             {
-                button1.PerformClick();
-            }
-        } 
 
-        public static readonly int maxFailedLoginAttempts = 3;
-
-        bool userNameIsValid = false;
-        bool passwordIsValid = false;
-        int failedLoginAttempts = 0;
-        int failedPassAttempts = 0;
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-            
-            string inputUsername = textBox1.Text;
-            string inputPassword = textBox2.Text;
-
-            if (failedLoginAttempts < maxFailedLoginAttempts)
-            {
-                int indexOfUserName = getUsernameIndex(inputUsername);
-                if (indexOfUserName > -1)
-                {
-                    userNameIsValid = true;
-                }
-
-
-                if (userNameIsValid == true)
-                {
-
-                    if (failedPassAttempts < maxFailedLoginAttempts)
-                    {
-                        passwordIsValid = isValidPassword(indexOfUserName, inputPassword);
-                        if (passwordIsValid)
-                        {
-                            MessageBox.Show("Login correct");
-                            Application.Exit();
-                        }
-                        else
-                        {
-                            failedPassAttempts += 1;
-                            MessageBox.Show("Password is incorrect. Please try again:");
-                            textBox2.Text = String.Empty;
-                            textBox2.Focus();
-                        }
-                    }
-                    else
-                    {
-                        Application.Exit();
-                    }
-                }
-                else
-                {
-                    failedLoginAttempts += 1;
-                    MessageBox.Show("Username is not in database. Please try again:");
-                    textBox1.Text = String.Empty;
-                    textBox2.Text = String.Empty;
-                    textBox1.Focus();
-                }
+               if (failedPassAttempts < maxFailedLoginAttempts)
+               {
+                  passwordIsValid = isValidPassword(indexOfUserName, inputPassword);
+                  if (passwordIsValid)
+                  {
+                     MessageBox.Show("Login correct");
+                     Application.Exit();
+                  }
+                  else
+                  {
+                     MessageBox.Show("Password is incorrect. Please try again:");
+                     textBox2.Text = String.Empty;
+                     textBox2.Focus();
+                     failedPassAttempts += 1;
+                  }
+               }
+               else
+               {
+                  Application.Exit();
+               }
             }
             else
-            {
-                Application.Exit();
+            {               
+               MessageBox.Show("Username is not in database. Please try again:");
+               textBox1.Text = String.Empty;
+               textBox2.Text = String.Empty;
+               textBox1.Focus();
+               failedLoginAttempts += 1;
             }
-            
-        }
+         }
+         else
+         {
+            Application.Exit();
+         }
+
+      }
 
 
-        public static int getUsernameIndex(string name)
-        {
-            string[][] users = GetUserPasswordArray();
+      public static int getUsernameIndex(string name)
+      {
+         string[][] users = GetUserPasswordArray();
 
-            int positionInArray = -1;
+         int positionInArray = -1;
 
-            for (int i = 0; i < users.Length; i++)
+         for (int i = 0; i < users.Length; i++)
+         {
+            for (int j = 0; j < users[i].Length; j++)
             {
-                for (int j = 0; j < users[i].Length; j++)
-                {
-                    if (users[i][j] == name)
-                    {
-                        positionInArray = i;
-                        break;
-                    }
-                }
-                if (positionInArray > -1)
-                {
-                    break;
-                }
+               if (users[i][j] == name)
+               {
+                  positionInArray = i;
+                  break;
+               }
             }
-            return positionInArray;
-        }
-
-
-        public static bool isValidPassword(int userIndx, string pass)
-        {
-            string[][] users = GetUserPasswordArray();
-
-            string passInDb = users[userIndx][1];
-            if (passInDb == pass)
+            if (positionInArray > -1)
             {
-                return true;
+               break;
             }
-            else
-            {
-                return false;
-            }
-        }
-
-        private static string[][] GetUserPasswordArray()
-        {
-            string[][] arr = new string[3][];
-
-            arr[0] = new string[2];
-            arr[0][0] = "kostovsk";
-            arr[0][1] = "123";
-
-            arr[1] = new string[2];
-            arr[1][0] = "skostov";
-            arr[1][1] = "456";
+         }
+         return positionInArray;
+      }
 
 
-            arr[2] = new string[2];
-            arr[2][0] = "stoichokostov";
-            arr[2][1] = "789";
+      public static bool isValidPassword(int userIndx, string pass)
+      {
+         string[][] users = GetUserPasswordArray();
 
-            return arr;
-        }
-    }
+         string passInDb = users[userIndx][1];
+         if (passInDb == pass)
+         {
+            return true;
+         }
+         else
+         {
+            return false;
+         }
+      }
+
+      private static string[][] GetUserPasswordArray()
+      {
+         string[][] arr = new string[3][];
+
+         arr[0] = new string[2];
+         arr[0][0] = "kostovsk";
+         arr[0][1] = "123";
+
+         arr[1] = new string[2];
+         arr[1][0] = "skostov";
+         arr[1][1] = "456";
+
+
+         arr[2] = new string[2];
+         arr[2][0] = "stoichokostov";
+         arr[2][1] = "789";
+
+         return arr;
+      }
+   }
 }
