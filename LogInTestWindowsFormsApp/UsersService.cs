0b5bc7774dadcdf5780a4e9d82ad877158c1d11b
Michael Kostov <kostovmichael@yahoo.com>
2020-06-08 12:48:36 -0700

    minor cleanup

--- LogInTestWindowsFormsApp/UsersService.cs
+++ LogInTestWindowsFormsApp/UsersService.cs
@@ -20,16 +20,13 @@ namespace LogInTestWindowsFormsApp
             List<User> theDeserializedListOfUsers = JsonConvert.DeserializeObject<List<User>>(jsonText);
             return theDeserializedListOfUsers;
          }
-         return null;
       }
 
       public void SaveUsers(List<User> userList)
       {
-         var test = Path.GetDirectoryName(Assembly.GetExecutingAssembly().CodeBase);
          using (StreamWriter file = File.CreateText(@"..\..\Data\users.json"))
          {
             JsonSerializer serializer = new JsonSerializer();
-            //serialize object directly into file stream
             serializer.Serialize(file, userList);
          }
       }
